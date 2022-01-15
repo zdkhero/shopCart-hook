dc83ae1c410f71c54ca823a11146e20869dbabf7
FlightLoong <1577886278@qq.com>
2022-01-15 17:41:42 +0800

    封装GoodsItem组件

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.scss'
 import CartHeader from './components/CartHeader'
 // 导入底部组件
 import CartFooter from './components/CartFooter'
+// 导入详情页组件
+import GoodsItem from './components/GoodsItem'
 
 
 export default function App() {
@@ -13,30 +15,9 @@ export default function App() {
       <CartHeader>购物车</CartHeader>
 
       {/* 商品列表项 */}
-      <div className="my-goods-item">
-        <div className="left">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="input"
-            />
-            <label className="custom-control-label" htmlFor="input">
-              <img
-                src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-                alt=""
-              />
-            </label>
-          </div>
-        </div>
-        <div className="right">
-          <div className="top">商品名称</div>
-          <div className="bottom">
-            <span className="price">¥ 商品价格</span>
-            <span>counter组件</span>
-          </div>
-        </div>
-      </div>
+      <GoodsItem></GoodsItem>
+      <GoodsItem></GoodsItem>
+      <GoodsItem></GoodsItem>
 
       {/* 底部 */}
       <CartFooter></CartFooter>
