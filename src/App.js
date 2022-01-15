b77f01ef1ee26863dfc84fa37d7a1f6fc7b97b93
FlightLoong <1577886278@qq.com>
2022-01-15 17:53:11 +0800

    商品列表渲染

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react'
+
+// 导入 aioxs
+import request from './utils/request'
+
 import './App.scss'
 
 // 导入头部组件 
@@ -7,17 +12,28 @@ import CartFooter from './components/CartFooter'
 // 导入详情页组件
 import GoodsItem from './components/GoodsItem'
 
-
 export default function App() {
+  const [goodsList, setGoodsList] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await request.get('/goodsList')
+      setGoodsList(res.data)
+    }
+    loadData()
+  }, [])
+
   return (
     <div className="app">
       {/* 标题 */}
       <CartHeader>购物车</CartHeader>
 
       {/* 商品列表项 */}
-      <GoodsItem></GoodsItem>
-      <GoodsItem></GoodsItem>
-      <GoodsItem></GoodsItem>
+      {
+        goodsList.map(item => (
+          <GoodsItem key={item.id} {...item} />
+        ))
+      }
 
       {/* 底部 */}
       <CartFooter></CartFooter>
